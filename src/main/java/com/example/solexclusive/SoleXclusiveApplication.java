4daf890725ea59278bf3803cb94e61c5a98e7e53
Fangyiming <fangyiming12354@alumnos.ilerna.com>
2026-03-23 16:20:16 +0100

    feat: inicio del proyecto Spring Boot

---
+++ src/main/java/com/example/solexclusive/SoleXclusiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.solexclusive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoleXclusiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoleXclusiveApplication.class, args);
+    }
+
+}
